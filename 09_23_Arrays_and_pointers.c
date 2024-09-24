d10ad871b8f55e99f8b410e38a7e116c96a9ef62
RyanRanjith <kingranjith4@gmail.com>
2024-09-23 21:15:31 -0700

    a

---
+++ 09_23_Arrays_and_pointers.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main() {
+
+    int a[5] = {0,1,2,3,4};
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d = %p\n", a[i], &a[i]);
+    }
+
+   // Printing only address of array
+    printf("Array Address: %p\n",a);
+
+    // We can use Pointers to print address of other numbers
+    printf("Array Address of 0: %p\n", a);
+    printf("Array address of 1: %p\n", a+1);
+    printf("Array address of 2: %p\n", a+2);
+    
+    return 0;
+}
+
+// Alternate way instead of using &a[i] we can use a + i
+
+#include <stdio.h>
+
+int main() {
+
+    int a[3] = {0,1,2};
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%d = %p\n", a[i], a + i);
+    }
+    
+
+    return 0;
+}
+
+// Accessing numbers using pointers
+#include <stdio.h>
+
+int main() {
+    
+    int a[3] = {0,1,2};
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%d = %p\n", *(a + i), a + i);
+    }
+    
+
+    return 0;
+}
